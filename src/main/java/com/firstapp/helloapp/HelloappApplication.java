1886b1fac8435dc6e2d1e8a47fcf9ba43a3611ec
fiqriismail <fiqri.ismail@gmail.com>
2023-02-04 14:18:05 +0530

    Added two endpoints

--- src/main/java/com/firstapp/helloapp/HelloappApplication.java
+++ src/main/java/com/firstapp/helloapp/HelloappApplication.java
@@ -2,12 +2,25 @@ package com.firstapp.helloapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloappApplication.class, args);
+	}
+
+	@GetMapping
+	public String index() {
+		return "Hello Spring Azure";
+	}
+
+	@GetMapping("/about")
+	public String about() {
+		return "We are at Azure";
 	}
 
 }
